320f1f67515188964c40d4e144505922e32f25b7
Anthony Gress <the.techx.guy@gmail.com>
2021-07-28 19:59:29 -0400

    fix cascading delete error

--- models/index.js
+++ models/index.js
@@ -7,10 +7,12 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "id",
+  onDelete: "CASCADE",
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
--- notes.md
+++ notes.md
@@ -8,3 +8,27 @@
 "stock": 3,
 "tagIds": [1, 2, 3, 4]
 }
+
+/products
+/categories
+/tags
+
+cd db && mysql -u root -p
+
+source schema.sql
+exit
+cd..
+npm run seed
+npm run start
+
+{
+"product_name": "Keyboard",
+"price": 60.00,
+"stock": 5,
+"tagIds": [5, 2, 1, 4],
+"category_id": 1
+}
+
+{
+"name": "Headphones"
+}
